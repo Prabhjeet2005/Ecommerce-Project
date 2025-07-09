141764970ea1cade6b6920616e3052013330d5d9
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-07-09 11:25:51 +0530

    feat: enhance user model with cart functionality and product references

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -4,6 +4,7 @@ const {
 	Types: { Decimal128 },
 } = require("mongoose");
 const { errorCreator } = require("../utils/responseHandler");
+const ProductModel = require("./ProductModel");
 
 const userSchema = new Schema(
 	{
@@ -25,8 +26,14 @@ const userSchema = new Schema(
 			type: String,
 			required: [true, "Password is Mandatory !"],
 		},
+		role: {
+			type: String,
+			enum: ["admin", "user"],
+			default: "user",
+		},
 		totalCount: {
 			type: Number,
+			default: 0,
 		},
 		totalValue: {
 			type: Decimal128,
@@ -34,7 +41,20 @@ const userSchema = new Schema(
 			get: (value) => parseFloat(value),
 			default: 0,
 		},
-		cart: [Object],
+		cart: [
+			{
+				product: {
+					// Stores just the ID
+					type: Schema.Types.ObjectId,
+					ref: "product",
+					required: true,
+				},
+				quantity: {
+					type: Number,
+					default: 1,
+				},
+			},
+		],
 	},
 	{
 		toObject: { getters: true },
@@ -43,7 +63,7 @@ const userSchema = new Schema(
 
 userSchema.statics.createUser = async (userData) => {
 	const { username } = userData;
-	const duplicateUser = await UserModel.findUser(username);
+	const duplicateUser = await UserModel.findOne({ username });
 	if (duplicateUser) {
 		errorCreator("User Already Exists", 401);
 	}
@@ -78,16 +98,34 @@ const sanitizedUserData = (userData) => {
 	return data;
 };
 
+/*
+_id -> findById => MongoDB ID
+id -> CustomId -> Product Model Unique Id
+*/
+
 // const {quantity} = await UserModel.findOne({username,"cart.id":product.id})
-userSchema.statics.addToCart = async (product, username) => {
+userSchema.statics.addToCart = async (username, _id) => {
+	const product = await ProductModel.findById(_id); // MongoDB ID
+	if (!product) {
+		errorCreator("Product Doesn't Exist", 400);
+	}
+
+	const user = await UserModel.findOne({ username });
+	const alreadyAddedToCart = user.cart.find(
+		(item) => item.product.toString() === _id.toString()
+	);
+	if (alreadyAddedToCart) {
+		return await UserModel.increment(username, _id);
+	}
+
 	const userData = await UserModel.findOneAndUpdate(
 		{ username },
 		{
-			$push: { cart: { ...product, quantity: 1 } },
+			$push: { cart: { product: product._id, quantity: 1 } },
 			$inc: { totalCount: 1, totalValue: product.price },
 		},
 		{ new: true }
-	);
+	).populate("cart.product");
 	return sanitizedUserData(userData);
 };
 
@@ -97,7 +135,7 @@ userSchema.statics.getCartItems = async (username) => {
 			username,
 		},
 		{ cart: 1, totalCount: 1, totalValue: 1 }
-	);
+	).populate("cart.product");
 
 	return sanitizedUserData(data);
 };
@@ -113,76 +151,106 @@ userSchema.statics.clearCart = async (username) => {
 
 // RETRIVING cart.id [NO $ REQUIRED]
 // UPDATING cart.$.quantity [$ REQUIRED]
-userSchema.statics.increment = async (username, product) => {
+userSchema.statics.increment = async (username, _id) => {
+	const exisitingProduct = await ProductModel.findById(_id);
+	if (!exisitingProduct) {
+		errorCreator("Product Doesn't Exist", 400);
+	}
+	const user = await UserModel.findOne({ username });
+	const alreadyAddedToCart = user.cart.find(
+		item => item.product._id.toString() === _id.toString()
+	)
+	if(!alreadyAddedToCart){
+		return await UserModel.addToCart(username,_id)
+	}
 	const userData = await UserModel.findOneAndUpdate(
-		{ username, "cart.id": product.id },
+		{ username, "cart.product": _id },
 		{
 			$inc: {
 				"cart.$.quantity": 1,
 				totalCount: 1,
-				totalValue: product.price,
+				totalValue: exisitingProduct.price,
 			},
 		},
 		{ new: true }
-	);
+	).populate("cart.product");
 	return sanitizedUserData(userData);
 };
 
-userSchema.statics.decrement = async (username, product) => {
+userSchema.statics.decrement = async (username, _id) => {
+	const exisitingProduct = await ProductModel.findById(_id);
+	if (!exisitingProduct) {
+		errorCreator("Product Doesn't Exist", 404);
+	}
 	//BACKEND Find Quantity
 	const user = await UserModel.findOne({
 		username,
-		"cart.id": product.id,
-	});
-
-	const cartItem = user.cart.find((e) => e.id === product.id);
-	const quantity  = cartItem?.quantity;
+		"cart.product": _id,
+	}).populate("cart.product");
+	
+	if(!user){
+		errorCreator("Cart Is Empty",404)
+	}
+	const cartItem = user.cart.find(
+		(item) => item.product._id.toString() === _id.toString()
+	);
+	const quantity = cartItem?.quantity;
 
 	if (quantity <= 1) {
-		const data = await UserModel.removeFromCart(username, product);
-		return sanitizedUserData(data);
+		return await UserModel.removeFromCart(username, _id);
 	}
+
 	const userData = await UserModel.findOneAndUpdate(
 		{
 			username,
-			"cart.id": product.id,
+			"cart.product": _id,
 		},
 		{
 			$inc: {
 				"cart.$.quantity": -1,
 				totalCount: -1,
-				totalValue: -product.price,
+				totalValue: -exisitingProduct.price,
 			},
 		},
 		{ new: true }
-	);
+	).populate("cart.product");
 	return sanitizedUserData(userData);
 };
 
-userSchema.statics.removeFromCart = async (username, product) => {
+userSchema.statics.removeFromCart = async (username, _id) => {
+	const existingProduct = await ProductModel.findById(_id);
+	if (!existingProduct) {
+		errorCreator("Product Doesn't Exist", 404);
+	}
 	const user = await UserModel.findOne({
 		username,
-		"cart.id": product.id,
+		"cart.product": _id,
 	});
 
-	const cartItem = user.cart.find((e) => e.id === product.id);
-	const quantity = cartItem?.quantity;
+	if(!user){
+		errorCreator("Cart is Empty",400)
+	}
+
+	const cartItem = user.cart.find(
+		(item) => item.product._id.toString() === _id.toString()
+	);
+	const cartItemQty = cartItem?.quantity;
 
 	const userData = await UserModel.findOneAndUpdate(
 		{ username },
 		{
 			$inc: {
-				totalCount: -quantity,
-				totalValue: -product.price * quantity,
+				totalCount: -cartItemQty,
+				totalValue: -existingProduct.price * cartItemQty,
 			},
 			$pull: {
-				cart: { id: product.id },
+				cart: { product:_id },
 			},
 		},
 		{ new: true }
-	);
+	).populate("cart.product");
 	return sanitizedUserData(userData);
 };
-
 const UserModel = model("users", userSchema);
+
 module.exports = UserModel;
